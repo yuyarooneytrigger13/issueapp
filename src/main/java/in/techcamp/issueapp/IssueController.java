c4d42d99ed9c7bf75c2c2d2d6cf7a334b09881b2
yuyarooneytrigger13 <yuya.rooney.trigger13@gmail.com>
2026-01-21 18:57:23 +0900

    保存機能作成

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -1,15 +1,35 @@
 package in.techcamp.issueapp;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@AllArgsConstructor
 public class IssueController {
+
+        private final IssueRepository issueRepository;
 
         @GetMapping("/issueForm")
         public String showIssueForm(@ModelAttribute("issueForm")IssueForm form){
             return "issueForm";
         }
+
+        @PostMapping("/issues")
+        public String createIssue(IssueForm issueForm,
+                                  Model model){
+            try {
+                issueRepository.insert(issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
+            } catch (Exception e) {
+                model.addAttribute("errorMessage", e.getMessage());
+                return "error";
+            }
+            return "redirect:/";
+        }
+
+
     }
 
