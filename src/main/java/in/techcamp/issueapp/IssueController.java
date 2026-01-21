aca38a78a60e2343f1ffa3a88789392cb2a87754
yuyarooneytrigger13 <yuya.rooney.trigger13@gmail.com>
2026-01-21 20:01:22 +0900

    更新機能

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -6,36 +6,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
 public class IssueController {
 
-        private final IssueRepository issueRepository;
+    private final IssueRepository issueRepository;
 
-        @GetMapping("/issueForm")
-        public String showIssueForm(@ModelAttribute("issueForm")IssueForm form){
-            return "issueForm";
-        }
-
-        @PostMapping("/issues")
-        public String createIssue(IssueForm issueForm,
-                                  Model model){
-            try {
-                issueRepository.insert(issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
-            } catch (Exception e) {
-                model.addAttribute("errorMessage", e.getMessage());
-                return "error";
-            }
-            return "redirect:/";
-        }
-
-        @GetMapping
-        public String showIssues(Model model){
-            var issueList = issueRepository.findAll();
-            model.addAttribute("issueList", issueList);
-            return "index";
-        }
+    @GetMapping("/issueForm")
+    public String showIssueForm(@ModelAttribute("issueForm")IssueForm form){
+        return "issueForm";
     }
+
+    @PostMapping("/issues")
+    public String createIssue(IssueForm issueForm,
+                                  Model model){
+        try {
+            issueRepository.insert(issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "error";
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping
+    public String showIssues(Model model){
+        var issueList = issueRepository.findAll();
+        model.addAttribute("issueList", issueList);
+        return "index";
+    }
+
+    @GetMapping("/issues/{id}")
+    public String issueDetail(@PathVariable long id, Model model){
+        var issue = issueRepository.findById(id);
+        model.addAttribute("issue", issue);
+        return "detail";
+    }
+
+    @PostMapping("/issues/{id}/update")
+    public String updateIssue(@PathVariable long id, IssueForm issueForm){
+        issueRepository.update(id, issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
+        return "redirect:/";
+    }
+}
 
--- src/main/java/in/techcamp/issueapp/IssueRepository.java
+++ src/main/java/in/techcamp/issueapp/IssueRepository.java
@@ -3,6 +3,7 @@ package in.techcamp.issueapp;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -13,4 +14,11 @@ public interface IssueRepository {
 
     @Select("select * from issues")
     List<IssueEntity> findAll();
+
+    @Select("select * from issues where id = #{id}")
+    IssueEntity findById(long id);
+
+    @Update("UPDATE issues SET title = #{title}, content = #{content}, period = #{period}, importance = #{importance} WHERE id =#{id}")
+    void update(long id, String title, String content, String period, Character importance);
 }
+
