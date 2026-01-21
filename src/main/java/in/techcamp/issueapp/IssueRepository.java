e15ad41f584e4dd6a0b8d403cb24dc426afc9674
yuyarooneytrigger13 <yuya.rooney.trigger13@gmail.com>
2026-01-21 19:36:07 +0900

    一覧表示機能

--- src/main/java/in/techcamp/issueapp/IssueRepository.java
+++ src/main/java/in/techcamp/issueapp/IssueRepository.java
@@ -2,9 +2,15 @@ package in.techcamp.issueapp;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface IssueRepository {
     @Insert("insert into issues (title, content, period, importance) values (#{title}, #{content}, #{period}, #{importance})")
     void insert(String title, String content, String period, Character importance);
+
+    @Select("select * from issues")
+    List<IssueEntity> findAll();
 }
